46c3cf0144fc350ce1f3e60aae058e9310d00f94
Thi Nguyen <tttn.tn13@gmail.com>
2021-12-15 19:47:49 -0800

    create logging

--- api/client/src/config/config.js
+++ api/client/src/config/config.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-});
+const axiosInstance = createAxiosInstance();
+
+const createAxiosInstance = () => {
+  console.log(
+    'Initializing Axios Instance with: ' + process.env.REACT_APP_BASE_URL
+  );
+  return axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+  });
+};
 export default axiosInstance;
