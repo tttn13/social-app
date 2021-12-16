0d4b7ebb1f0c56037ef3dfdd59b2a9878640d59f
Thi Nguyen <tttn.tn13@gmail.com>
2021-12-15 19:42:03 -0800

    change app name

--- api/client/src/config/config.js
+++ api/client/src/config/config.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const axiosInstance = axios.create({
-    baseURL : process.env.REACT_APP_BASE_URL
-})
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
 export default axiosInstance;
