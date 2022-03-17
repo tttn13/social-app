5d642aeb65bfa352de71e37ed903cfd186075a89
Thi Nguyen <tttn.tn13@gmail.com>
2022-03-17 11:55:09 -0700

    create a container component for location suggestions

---
+++ api/client/src/components/share/OptionsModalBox/PlacesSuggestions.js
@@ -0,0 +1,56 @@
+import { LocationOn, Search } from "@mui/icons-material";
+import { Combobox, ComboboxInput, ComboboxList } from "@reach/combobox";
+
+const PlacesSuggestions = (results) => {
+  const { value, ready, status, data, handleInput, handleSelect, handleClick } = results;
+  const renderSuggestions = () => {
+    const suggestions = data.map(({ place_id, description }) => (
+      <li
+        key={place_id}
+        className="locationListItem"
+        onClick={() => handleClick({ place_id, description })}
+      >
+        <LocationOn />
+        {description}
+      </li>
+    ));
+
+    return (
+      <>
+        {suggestions}
+        <li className="locationListItem">
+          <img
+            src="https://developers.google.com/maps/documentation/images/powered_by_google_on_white.png"
+            alt=""
+          />
+        </li>
+      </>
+    );
+  };
+
+  return (
+    <Combobox
+      onSelect={handleSelect}
+      aria-labelledby="demo"
+      className="locationModalSearchBox"
+    >
+      <div className="locationModalSearch">
+        <Search className="locationModalSearchIcon" type="submit" />
+        <ComboboxInput
+          className="locationSearchInput"
+          style={{ width: 300, maxWidth: "90%" }}
+          value={value}
+          onChange={handleInput}
+          placeholder="Where are you ?"
+          disabled={!ready}
+        />
+      </div>
+
+      <ComboboxList className="locationList">
+        {status === "OK" && renderSuggestions()}
+      </ComboboxList>
+    </Combobox>
+  );
+};
+
+export default PlacesSuggestions;
--- api/client/src/components/share/OptionsModalBox/SearchLocationModal.js
+++ api/client/src/components/share/OptionsModalBox/SearchLocationModal.js
@@ -6,13 +6,13 @@ import { Combobox, ComboboxInput, ComboboxList } from "@reach/combobox";
 import { useRef } from "react";
 import usePlacesAutocomplete from "use-places-autocomplete";
 import { useOutsideAlerter } from "../../customHooks/useOutsideAlerter";
+import PlacesSuggestions from "./PlacesSuggestions";
 
 const SearchLocationModal = ({
   setLocationModalActive,
   updateLocation,
   locationModalActive,
 }) => {
-  
   const {
     ready,
     value,
@@ -20,61 +20,71 @@ const SearchLocationModal = ({
     setValue,
     clearSuggestions,
   } = usePlacesAutocomplete();
-  const locationModalContent = useRef()
+
+  const locationModalContent = useRef();
   const handleInput = (e) => {
     setValue(e.target.value);
   };
-  
+
   const handleSelect = (val) => {
     setValue(val, false);
   };
-  
+
   const handleClick = ({ place_id, description }) => {
     setLocationModalActive(false);
     updateLocation(description.toString());
     clearSuggestions();
   };
-  const ErrorComponent = () => (<div>Error</div>)
-  const Spinner = () => (<div>Spinning</div>)
-  const renderMapsApi = (status) => {
-    switch (status) {
+  const ErrorComponent = () => <div>Error</div>;
+  const Spinner = () => <div>Spinning</div>;
+  const renderMapsApi = (scriptStatus) => {
+    switch (scriptStatus) {
       case Status.LOADING:
         return <Spinner />;
       case Status.FAILURE:
         return <ErrorComponent />;
       case Status.SUCCESS:
-        return <Places />;
+        const results = {
+          value: value,
+          ready: ready,
+          status: status,
+          data: data,
+          handleInput,
+          handleSelect,
+          handleClick,
+        };
+        return <PlacesSuggestions results={results} />;
     }
   };
-  const renderSuggestions = () => {
-    const suggestions = data.map(({ place_id, description }) => (
-      <li
-        key={place_id}
-        className="locationListItem"
-        onClick={() => handleClick({ place_id, description })}
-      >
-        <LocationOn />
-        {description}
-      </li>
-    ));
+  // const renderSuggestions = () => {
+  //   const suggestions = data.map(({ place_id, description }) => (
+  //     <li
+  //       key={place_id}
+  //       className="locationListItem"
+  //       onClick={() => handleClick({ place_id, description })}
+  //     >
+  //       <LocationOn />
+  //       {description}
+  //     </li>
+  //   ));
 
-    return (
-      <>
-        {suggestions}
-        <li className="locationListItem">
-          <img
-            src="https://developers.google.com/maps/documentation/images/powered_by_google_on_white.png"
-            alt=""
-          />
-        </li>
-      </>
-    );
-  };
-  
-  useOutsideAlerter({ 
-    ref: locationModalContent, 
-    setModalActive: setLocationModalActive 
-  })
+  //   return (
+  //     <>
+  //       {suggestions}
+  //       <li className="locationListItem">
+  //         <img
+  //           src="https://developers.google.com/maps/documentation/images/powered_by_google_on_white.png"
+  //           alt=""
+  //         />
+  //       </li>
+  //     </>
+  //   );
+  // };
+
+  useOutsideAlerter({
+    ref: locationModalContent,
+    setModalActive: setLocationModalActive,
+  });
 
   return (
     <div
@@ -91,12 +101,13 @@ const SearchLocationModal = ({
           />
         </div>
 
-        <Wrapper 
-          apiKey={process.env.REACT_APP_MAPS_API_KEY}
-          libraries={["places"]}
-          render={renderMapsApi}
-        >
-          <Combobox
+        {locationModalActive && (
+          <Wrapper
+            apiKey={process.env.REACT_APP_MAPS_API_KEY}
+            libraries={["places"]}
+            render={renderMapsApi}
+          >
+            {/* <Combobox
             onSelect={handleSelect}
             aria-labelledby="demo"
             className="locationModalSearchBox"
@@ -116,8 +127,9 @@ const SearchLocationModal = ({
             <ComboboxList className="locationList">
               {status === "OK" && renderSuggestions()}
             </ComboboxList>
-          </Combobox>
-        </Wrapper>
+          </Combobox> */}
+          </Wrapper>
+        )}
       </div>
     </div>
   );
